b9c167d9a2dbe18cb4206b3ae7dac6dedc1eef85
mariamartynova <marymartballet@yandex.ru>
2021-09-18 13:45:42 +0300

    feature: added new game

---
+++ src/gcd.js
@@ -0,0 +1,22 @@
+import getRandomInt from './utils.js';
+import runEngine from './index.js';
+
+const gcd = (num1, num2) => {
+  const minNum = Math.min(num1, num2);
+  for (let divider = minNum; divider > 1; divider -= 1) {
+    if (num1 % divider === 0 && num2 % divider === 0) {
+      return divider;
+    }
+  }
+  return 1;
+};
+
+const genRoundData = () => {
+  const num1 = getRandomInt();
+  const num2 = getRandomInt();
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(gcd(num1, num2));
+  return { correctAnswer, question };
+};
+
+export default () => runEngine(genRoundData, 'Find the greatest common divisor of given numbers.');
